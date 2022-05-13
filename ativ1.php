7e8fcc09a65265df836e256bff4e44302836a860
ravenaRRR <raphaelamirandola@hotmail.com>
2022-05-13 08:49:17 -0300

    commit1

---
+++ ativ1.php
@@ -0,0 +1,65 @@
+<?php
+
+$estado = $_GET['estado'];
+
+
+switch($estado){
+
+case 'valor1':
+    case 'valor4':
+        case 'valor3':
+            case 'valor26':
+                case 'valor15':
+                    case 'valor21':
+                        case 'valor22':
+
+                            echo "Região Norte";
+
+                            break;
+
+                            case 'valor6':
+                                case 'valor2':
+                                    case 'valor5':
+                                        case 'valor9':
+                                            case 'valor13':
+                                                case 'valor16':
+                                                    case 'valor17':
+                                                        case 'valor19':
+                                                            case 'valor25':
+
+                            
+                                                        echo "Região Nordeste";
+                            
+                                                        break;
+                                                        
+                                                        
+                                                        case 'valor8':
+                                                            case 'valor10':
+                                                                case 'valor11':
+                                                                    case 'valor27':
+
+echo "Região Centro-Oeste";
+break;
+
+case 'valor7':
+case 'valor18':
+case 'valor3':
+case 'valor24':
+case 'valor12':
+case 'valor21':
+case 'valor22':
+echo "Região Sudeste";
+break;
+
+case 'valor15':
+case 'valor23':
+case 'valor20':
+echo "Região Sul";                                                                                                               
+break;
+
+
+
+
+
+
+}
