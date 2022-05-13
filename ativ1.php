dd83549046d912231741d053d4b4dc9dfbf94d49
ravenaRRR <raphaelamirandola@hotmail.com>
2022-05-13 08:52:14 -0300

    commit3

--- ativ1.php
+++ ativ1.php
@@ -6,38 +6,31 @@ $estado = $_GET['estado'];
 switch($estado){
 
 case 'valor1':
-    case 'valor4':
-        case 'valor3':
-            case 'valor26':
-                case 'valor15':
-                    case 'valor21':
-                        case 'valor22':
+case 'valor4':
+case 'valor3':
+case 'valor26':
+case 'valor15':
+case 'valor21':
+case 'valor22':
+echo "Região Norte";
+break;
 
-                            echo "Região Norte";
+case 'valor6':
+case 'valor2':
+case 'valor5':
+case 'valor9':
+case 'valor13':
+case 'valor16':
+case 'valor17':
+case 'valor19':
+case 'valor25':
+echo "Região Nordeste";
+break;
 
-                            break;
-
-                            case 'valor6':
-                                case 'valor2':
-                                    case 'valor5':
-                                        case 'valor9':
-                                            case 'valor13':
-                                                case 'valor16':
-                                                    case 'valor17':
-                                                        case 'valor19':
-                                                            case 'valor25':
-
-                            
-                                                        echo "Região Nordeste";
-                            
-                                                        break;
-                                                        
-                                                        
-                                                        case 'valor8':
-                                                            case 'valor10':
-                                                                case 'valor11':
-                                                                    case 'valor27':
-
+case 'valor8':
+case 'valor10':
+case 'valor11':
+case 'valor27':
 echo "Região Centro-Oeste";
 break;
 
